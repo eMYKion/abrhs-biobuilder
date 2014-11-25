6f93e2e0bb30646fb289cce65b2864df5789bc28
eMYKion <eMYKion@users.noreply.github.com>
2014-11-24 19:57:11 -0500

    improved object definitions

--- script/background.js
+++ script/background.js
@@ -1,19 +1,23 @@
-var fillCanvasBox = new rect(0,0,canvas.height,canvas.width,"#000000");
+var fillCanvasBox = new rect(new vector(0,0), new vector(canvas.height,canvas.width),"#000000");
 
-var model = new eColi( [ new point(50,100), new point(50,200) ] ,  [ new point(50,50), new point(50,150), new point(50,250) ] );
+var model = new eColi(
+  [ new point(50,20,"#ffffff"), new point(50,40,"#ffffff") ] ,  
+  [ new point(50,10,"#ffffff"), new point(50,30,"#ffffff"), new point(50,50,"#ffffff") ] 
+);
 
 var time = 0;
 
-var r = new point(50,50);
-
+var r = new point(100,100,"#ffff00");
+fillCanvasBox.draw();
 function render(){
+  
+  fillCanvasBox.draw();
   
   
   model.draw(time);
   r.draw();
-  fillCanvasBox.draw();
-  
   time+=1;
+  
   setTimeout(render, 1000/60);
 }
 
--- script/life.js
+++ script/life.js
@@ -2,11 +2,23 @@ function vector(x,y){
   this.x =x;
   this.y =y;
   
+  this.setpos = function(vec){
+    this.x=vec.x;
+    this.y=vec.y;
+  };
+  
 }
 
-function point(x,y){
-  this.x=x;
-  this.y=y;
+
+function point(x,y,color){
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  
+  this.setpos = function(vec){
+    this.x=vec.x;
+    this.y=vec.y;
+  };
   
   this.translate = function(vec){
     this.x+=vec.x;
@@ -14,23 +26,32 @@ function point(x,y){
   };
   
   this.draw = function(){
-    ctx.fillStyle="#ffffff";
-    ctx.fillRect(this.x, this.y, 50,50);
+    ctx.fillStyle=this.color;
+    ctx.fillRect(this.x, this.y, 1,1);
   }
   
 }
 
 
-function rect(a, b, h, w, color){
-  this.x = a;
-  this.y = b;
-  this.height = h;
-  this.width = w;
+function rect(vec1, vec2, color){
+  this.x = vec1.x;
+  this.y = vec1.y;
+  this.height = vec2.x;
+  this.width = vec2.y;
   this.color = color;
   
   this.draw = function(){
     ctx.fillStyle=this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+  
+  this.setpos = function(vec1,vec2){
+    this.x=vec1.x;
+    this.y=vec1.y;
+    this.width=vec2.x;
+    this.height=vec2.y;
+    
+    
   };
   
   this.translate = function(vec){
@@ -61,7 +82,11 @@ function eColi(fixed,major){
   this.major = [major[0],major[1],major[2]];
   
   this.draw = function(currTime){
-    console.log(Math.cos(currTime));
+    this.fixed[0].draw();
+    this.fixed[1].draw();
+    this.major[0].draw();
+    this.major[1].draw();
+    this.major[2].draw();
   };
 }
 
