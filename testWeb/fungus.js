e6649e72ed1ee3b1881fb327334a8940d568db6e
ab_igem <roshni714@gmail.com>
2015-02-02 15:45:55 +0000

    before meeting

--- testWeb/fungus.js
+++ testWeb/fungus.js
@@ -1,3 +1,12 @@
+//house keeping stuff
+var Banner = document.getElementById("Banner");
+function resize(){
+    var BannerWidth = document.getElementById("top").clientWidth;
+    Banner.width = BannerWidth-100;
+    Banner.height = BannerWidth/1277*274;
+}
+
+//and now the fun begins!
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
@@ -6,3 +15,10 @@ canvas.height  = canvas.width;
 ctx.strokeStyle = "#000000";
 ctx.arc(50,50,5,0,2*Math.PI, true);
 ctx.stroke();
+
+function render(){
+    resize();
+    setInterval(render, 1000/60);
+    
+}
+render();
